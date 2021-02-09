3aa2f1bb49bbfebfaa2cf4570d8eda80c388e554
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-02-09 16:59:42 -0300

    Adiciona estilo na unica pagina da aplicação

--- src/pages/MainPage/MainPageComponents.ts
+++ src/pages/MainPage/MainPageComponents.ts
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
   align-items: center;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  padding: 30px;
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+`;
+
+export const CardsWrapper = styled.div`
+  margin: 20px 0;
+  width: 100%;
+  height: 250px;
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+
 `;
