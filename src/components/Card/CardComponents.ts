6f78de28a48016f56da9d45265e03213ca6441f5
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-02-09 17:38:08 -0300

    Termina o layout das cards dentro da aplicação.

--- src/components/Card/CardComponents.ts
+++ src/components/Card/CardComponents.ts
@@ -1,3 +1,56 @@
 import styled from 'styled-components'
+import CountUp from 'react-countup';
 
-export const Container = styled.div``;
+interface CardColor {
+  type: 'INFECTED' | 'RECOVERED' | 'DEATH';
+}
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CardBody = styled.div<CardColor>`
+  width: 250px;
+  height: 210px;
+  padding: 15px;
+  background-color: white;
+  border-radius: 5px;
+  
+  border-bottom: ${(props) => {
+      switch (props.type) {
+        case 'INFECTED':
+          return '10px solid rgba(0, 0, 255, 0.5)';
+        case 'RECOVERED':
+          return '10px solid rgba(0, 255, 0, 0.5)';
+        default:
+          return '10px solid rgba(255, 0, 0, 0.5)';
+      }
+  }};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  box-shadow: 6px 11px 17px -7px rgba(168,168,168,0.73);
+`
+
+export const CardRegularText = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  color: gray;
+`
+
+export const CardNumber = styled(CountUp)`
+  font-size: 22px;
+  font-weight: 600;
+  color: black;
+`
+
+export const CardInfo = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+`;
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { CovidNumbers } from '../../dtos/ICovidApi';
+import { CardBody, CardInfo, CardNumber, CardRegularText, Container } from './CardComponents';
 
-const Cards:React.FC = () => {
+
+interface CardProps {
+  covidNumbers: CovidNumbers;
+  updatedDate: string;
+  type: 'INFECTED' | 'RECOVERED' | 'DEATH';
+}
+
+const Card:React.FC<CardProps> = ({ covidNumbers, updatedDate, type }) => {
+  
+  const typeOfCard = 
+    type === 'RECOVERED'? 
+      "Recuperados" : 
+      type === "INFECTED" ? "Infectados": "Mortos";
+
+  const message = type === 'RECOVERED'? 
+    "Número de pessoas recuperadas do COVID-19" : 
+    type === "INFECTED" ? "Número de infectados pelo COVID-19": "Número de mortos pelo COVID-19";
+
   return (
-    <h1>Cards</h1>
+
+    <Container>
+      <CardBody type={type}>
+        <CardRegularText>{typeOfCard}</CardRegularText>
+        <CardNumber start={0} end={100} duration={1.5} separator=","></CardNumber>
+        <CardRegularText>DATA DA ULTIMA ATUALIZAÇÃO</CardRegularText>
+        <CardInfo>{message}</CardInfo>
+      </CardBody>
+    </Container>
   )
 }
 
-export default Cards;
+export default Card;
