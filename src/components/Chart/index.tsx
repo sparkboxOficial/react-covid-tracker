021710b28e045cf2d78be223b0b0b032f727a25f
Renan Mascarenhas Goncalves <renan.goncalves@bv.com.br>
2021-02-09 22:54:56 -0300

    Adiciona pacotes do chart.js necessários e alguns pequenos ajustes de css

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const Chart:React.FC = () => {
-  return (
-    <h1>Chart</h1>
-  )
+import { Bar } from 'react-chartjs-2';
+
+import ICovidApi from '../../dtos/ICovidApi';
+
+interface ChartDTO {
+  covidData?: ICovidApi;
+  country?: boolean;
+}
+
+const Chart:React.FC<ChartDTO> = ({ covidData, country }) => {
+
+  const barChart = <Bar 
+    data={{
+      labels: ['Infectados', 'Recuperados', 'Mortos'],
+      datasets: [
+        {
+          label: 'People',
+          backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+          data: [150, 200, 300]
+        }
+      ]
+    }}
+    options={{
+      legend: { display: false },
+      title: { display: true, text: `Current state no ` },
+    }}
+  />;
+
+  return ( barChart )
 }
 
 export default Chart;
